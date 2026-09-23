ddcd386947212f11d795973fa5c7bc40ef2d5d02
lob <coffeescript@kakao.com>
2026-09-23 10:49:06 +0900

    test: cover in-place grayscale alpha resize

--- test/transform/grayscale_alpha_resize_test.dart
+++ test/transform/grayscale_alpha_resize_test.dart
@@ -1,7 +1,7 @@
 import 'package:image/image.dart';
 import 'package:test/test.dart';
 
-Image _opaqueLa8x8({Format format = Format.uint8}) {
+Image _la100Alpha255({Format format = Format.uint8}) {
   final src = Image(width: 8, height: 8, numChannels: 2, format: format);
   for (final p in src) {
     p
@@ -11,7 +11,7 @@ Image _opaqueLa8x8({Format format = Format.uint8}) {
   return src;
 }
 
-void expectOpaqueLa100(Image image) {
+void _expectLa100Alpha255(Image image) {
   expect(image.numChannels, 2);
   for (final p in image) {
     expect(p.r, 100);
@@ -26,22 +26,40 @@ void main() {
         test(
             'copyResize ${interpolation.name} preserves grayscale alpha '
             'in $format', () {
-          final resized = copyResize(_opaqueLa8x8(format: format),
+          final resized = copyResize(_la100Alpha255(format: format),
               width: 4, interpolation: interpolation);
           expect(resized.width, 4);
           expect(resized.height, 4);
-          expectOpaqueLa100(resized);
+          _expectLa100Alpha255(resized);
+        });
+      }
+    }
+
+    for (final format in [Format.uint8, Format.uint16]) {
+      for (final interpolation in [
+        Interpolation.nearest,
+        Interpolation.average
+      ]) {
+        test(
+            'resize ${interpolation.name} preserves grayscale alpha samples '
+            'in-place in $format', () {
+          final src = _la100Alpha255(format: format);
+          final resized = resize(src, width: 4, interpolation: interpolation);
+          expect(identical(resized, src), isTrue);
+          expect(resized.width, 4);
+          expect(resized.height, 4);
+          _expectLa100Alpha255(resized);
         });
       }
     }
 
     test('copyResize after PNG round-trip preserves opaque grayscale alpha',
         () {
-      final bytes = encodePng(_opaqueLa8x8());
+      final bytes = encodePng(_la100Alpha255());
       final decoded = decodePng(bytes)!;
-      expectOpaqueLa100(decoded);
+      _expectLa100Alpha255(decoded);
       final resized = copyResize(decoded, width: 4);
-      expectOpaqueLa100(resized);
+      _expectLa100Alpha255(resized);
     });
   });
 }
